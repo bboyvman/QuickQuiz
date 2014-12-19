0fe92d678b5993791c48cb0db827980a6c7e849d
VartanArtinian <bboyvartan@gmail.com>
2014-12-19 09:58:40 -0800

    no message

---
+++ QuickQuiz/app/src/main/java/com/artinianv/quickquiz/Questions.java
@@ -0,0 +1,19 @@
+package com.artinianv.quickquiz;
+
+import android.app.Activity;
+
+/**
+ * Created by Student on 12/19/2014.
+ */
+public class Questions extends Activity {
+
+    public class stuff() {
+        q1 = "There is 24 hours in one day.";
+        q2 = "A 3D cube 8 sides.";
+        q3 = "I Failed this class";
+        a1 = "true";
+        a2 = "true";
+        a3 = "true";
+    }
+
+}
